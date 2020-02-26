8b24afd426aed83a63de81e23cb04f088fee231d
TonyKioko <tonykioko384@gmail.com>
2020-02-26 18:11:25 +0300

    created products seeder for testing purposes

--- database/migrations/2020_02_26_144514_create_products_table.php
+++ database/migrations/2020_02_26_144514_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('image');
             $table->decimal('price', 8, 2)->nullable();
             $table->text('description')->nullable();
             $table->unsignedInteger('quantity');
